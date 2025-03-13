fd3a19c4d8f0c99915d5eef24f332a5d6188d4b3
Waldemberg Pereira <waldemberg.pereira@outlook.com>
2025-03-13 08:32:15 -0300

    Atualizado a classe DaoUsuario

--- funcoes.py
+++ funcoes.py
@@ -4,7 +4,7 @@ from src.models.cliente import Cliente, StatusEnum
 from src.models.estoque_cliente import EstoqueCliente
 from src.models.frasco import Frasco
 from src.models.item_frasco import ItemFrasco
-from src.models.usuario import Usuario
+from src.models.usuario import Usuario, PermissaoEnum
 from src.models.historico_estoque import HistoricoEstoque
 from src.models.movimentacao import Movimentacao
 
@@ -13,15 +13,14 @@ from src.database.db import create_tables, drop_tables, create_session
 from src.dao.dao_cliente import DaoCliente
 from src.dao.dao_frasco import DaoFrasco
 from src.dao.dao_lembrete import DaoLembrete
+from src.dao.dao_usuario import DaoUsuario
 # from dao.dao_movimentacao import DaoSolicitacao
 # from src.dao.dao_frasco import DaoFrasco
 # from src.dao.dao_estoque_cliente import DaoEstoqueCliente
 # from src.dao.dao_historico_estoque import DaoHistoricoEstoque
-# from src.dao.dao_usuario import DaoUsuario
 # from src.dao.dao_estoque_movimentacao import DaoEstoqueMovimentacao
 
 # from src.controllers.controller_frasco import ControllerFrasco
 # from src.controllers.controller_cliente import ControllerCliente
 # from src.controllers.controller_solicitacao_estoque import ControllerSolicitacaoEstoque
 # from src.controllers.controller_estoque_cliente import ControllerEstoqueCliente
-
--- src/dao/dao_usuario.py
+++ src/dao/dao_usuario.py
@@ -1,14 +1,14 @@
-# from src.models.usuario import Usuario
+from src.models.usuario import Usuario
 
-# class DaoUsuario:
-#     @classmethod
-#     def criar_usuario(cls, session, login, senha, nome):
-#         usuario = Usuario(login = login, senha=senha, nome=nome)
-#         session.add(usuario)
-#         return usuario
+class DaoUsuario:
+    @classmethod
+    def criar_usuario(cls, session, login, senha, nome, permissao):
+        usuario = Usuario(login = login, senha=senha, nome=nome, permissao=permissao)
+        session.add(usuario)
+        return usuario
     
-#     @classmethod
-#     def obter_usuario_pelo_id(cls, session, id_usuario):
-#         usuario = session.query(Usuario).filter(Usuario.id == id_usuario).first()
-#         return usuario
+    @classmethod
+    def obter_usuario_pelo_id(cls, session, id_usuario):
+        usuario = session.query(Usuario).filter(Usuario.id == id_usuario).first()
+        return usuario
         
